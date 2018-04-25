fa7d3ee27f2172e6b1437f08dd8648c9a862dec9
Anthony Lam <al3101@nyu.edu>
2018-04-25 19:45:20 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -18,6 +18,13 @@ function destructivelyRemoveFirstKitten()  {
 
 function appendKitten(name) {
   var new_kittens = kittens
+  new_kittens.push(name)
+  return new_kittens
+}
+
+function prependKitten(name) {
+  var new_kittens = kittens
+  new_kittens.unshift(name)
+  return new_kittens
   
-  return new_kittens.push(name)
 }
