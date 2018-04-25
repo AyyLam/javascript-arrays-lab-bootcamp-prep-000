f34ee3bf83df7503a5f6baa098a330bbc58fdf33
Anthony Lam <al3101@nyu.edu>
2018-04-25 19:13:29 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 const app = "I don't do much."
 kittens = ["Milo", "Otis", "Garfield"]
 function destructivelyAppendKitten(name) { 
-  appendKitten() 
+  kittens.push(name) 
+}
+
+function destructivelyPrependKitten(name) {
+  kittens.unshift(name)
+}
+
+
   
